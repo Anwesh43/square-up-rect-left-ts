67a53464ed140bbd79cd3c96979cbe831de6112a
Anwesh43 <anweshthecool0@gmail.com>
2021-09-11 17:41:33 +0530

    adding hooks

---
+++ src/hooks.ts
@@ -0,0 +1,27 @@
+import {useState, useEffect} from 'react'
+
+const scGap : number = 0.02 
+const delay : number = 20 
+
+export const useAnimatedScale = () => {
+    const [scale, setScale] = useState(0)
+    const [animated, setAnimated] = useState(false)
+    return {
+        scale, 
+        start() {
+            if (!animated) {
+                setAnimated(true)
+                const interval = setInterval(() => {
+                    setScale((prev : number) => {
+                        if (prev > 1) {
+                            setAnimated(false)
+                            clearInterval(interval)
+                            return 0 
+                        }
+                        return prev + scGap 
+                    })
+                }, delay)
+            }
+        }
+    }
+}
