fc1345d31e7fa0e28d71c65c7ccdf448f74734af
Anwesh43 <anweshthecool0@gmail.com>
2021-09-11 23:39:21 +0530

    adding useDimension hoo

--- src/hooks.ts
+++ src/hooks.ts
@@ -25,3 +25,23 @@ export const useAnimatedScale = () => {
         }
     }
 }
+
+const useDimension = () => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        window.onresize = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+        return () => {
+            window.onresize = () => {
+                
+            }
+        }
+    })
+    return {
+        w, 
+        h, 
+    }
+}
