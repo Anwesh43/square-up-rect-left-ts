2042deaca6155454c93996117bc700a48252c9f4
Anwesh43 <anweshthecool0@gmail.com>
2021-09-12 01:32:07 +0530

    adding SquareUpRectLeft component

--- src/SquareUpRectLeft.tsx
+++ src/SquareUpRectLeft.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import withContext from './withContext'
 import {useStyle} from './hooks'
-
 interface SURLProps {
     w : number, 
     h : number, 
@@ -27,5 +26,5 @@ const SquareUpRectLeft = (props : SURLProps) => {
     )
 }
 
-export default SquareUpRectLeft
+export default withContext(SquareUpRectLeft)
 
